780daa98091ffd5f63cd7bbb228567bf33415237
Alexis de Lattre <alexis@via.ecp.fr>
2012-05-10 00:58:49 +0200

    Update the module description to add the new feature "open calling partner". Code clean-up.

--- asterisk_click2dial/__openerp__.py
+++ asterisk_click2dial/__openerp__.py
@@ -25,26 +25,26 @@
     'version': '0.3',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
-    'description': """This module adds a 'dial' button in the partner address
-view so that users can directly dial a phone number through Asterisk. This feature is usually known as 'click2dial'.
+    'description': """This module adds 3 functionnalities :
 
-Here is how it works :
-1) In OpenERP, the user clicks on the 'dial' button next to a phone number field in the Partner address view.
-2) Asterisk makes the user's phone ring.
-3) The user answers his own phone (if he doesn't, the process stops here).
-4) Asterisk dials the phone number found in OpenERP in place of the user.
-5) If the remote party answers, the user can talk to his correspondent.
+1) It adds a 'dial' button in the partner address form view so that users can directly dial a phone number through Asterisk. This feature is usually known as 'click2dial'. Here is how it works :
+. In OpenERP, the user clicks on the 'dial' button next to a phone number field in the partner address view.
+. OpenERP connects to the Asterisk Manager Interface and Asterisk makes the user's phone ring.
+. The user answers his own phone (if he doesn't, the process stops here).
+. Asterisk dials the phone number found in OpenERP in place of the user.
+. If the remote party answers, the user can talk to his correspondent.
 
-This module also adds the ability to show the name of the calling party on incoming phone calls if the presented
-phone number is present in the Partner addresses of OpenERP.
+2) It adds the ability to show the name of the calling party on the screen of your IP phone on incoming phone calls if the presented
+phone number is present in the partner addresses of OpenERP. Here is how it works :
+. On incoming phone calls, the Asterisk dialplan executes an AGI script "get_cid_name_timeout.sh".
+. The "get_cid_name_timeout.sh" script calls the "get_cid_name.py" script with a short timeout.
+. The "get_cid_name.py" script will make an XML-RPC request on the OpenERP server to try to find the name of the person corresponding to the phone number presented by the calling party.
+. If it finds the name, it is set as the CallerID name of the call, so as to be presented on the IP phone of the user.
 
-Here is how it works :
-1) On incoming phone calls, the Asterisk dialplan executes an AGI "get_cid_name_timeout.sh".
-2) The "get_cid_name_timeout.sh" script calls the "get_cid_name.py" script with a short timeout.
-3) The "get_cid_name.py" script will make an XML-RPC request on the OpenERP server to try to find the name
-   of the person corresponding to the phone number presented by the calling party.
-4) If it finds the name, it is add as CallerID name of the call, so as to be presented on the IP phone
-   of the user.
+3) It adds a button "Open calling partner" in the menu "Sales > Address book" to open the partner form of the calling party in 2 clicks. Here is how it works :
+. When the user clicks on the "Open calling partner" button, OpenERP sends a query to the Asterisk Manager Interface to get a list of the current phone calls
+. If it finds a phone call involving the user's phone, it gets the phone number of the calling party
+. It searches the phone number of the calling party in the Partner addresses of OpenERP and, if a record matches, it opens the form view of the corresponding partner.
 
 A detailed documentation for this module is available on the Akretion Web site : http://www.akretion.com/en/products-and-services/openerp-asterisk-voip-connector """,
     'author': 'Akretion',
