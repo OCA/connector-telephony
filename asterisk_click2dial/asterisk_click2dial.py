72631a3f1f563b42542c6e30858e9c9f7384aa9f
Alexis de Lattre <alexis@via.ecp.fr>
2012-06-03 01:59:26 +0200

    asterisk_click2dial_crm : - when you do a click2dial, it now triggers a wizard that proposes to create a CRM phone call (instead of directly creating the CRM phone call) - Add option on res_users for users who don't want to trigger the wizard when they do a click2dial - Add FR translation - update module description and module category
    asterisk_click2dial :
    - update module category

--- asterisk_click2dial/asterisk_click2dial.py
+++ asterisk_click2dial/asterisk_click2dial.py
@@ -489,9 +489,9 @@ class wizard_open_calling_partner(osv.osv_memory):
         '''Thanks to the default_get method, we are able to query Asterisk and
         get the corresponding partner when we launch the wizard'''
         res = {}
-        #calling_number = self.pool.get('asterisk.server')._connect_to_asterisk(cr, uid, method='get_calling_number', context=context)
+        calling_number = self.pool.get('asterisk.server')._connect_to_asterisk(cr, uid, method='get_calling_number', context=context)
         #To test the code without Asterisk server
-        calling_number = "0141981242"
+        #calling_number = "0141981242"
         if calling_number:
             res['calling_number'] = calling_number
             # We match only on the end of the phone number
