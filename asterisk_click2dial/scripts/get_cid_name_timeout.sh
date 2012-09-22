98454e92ebc4310877f62ba26876a1562937c8ae
Alexis de Lattre <alexis@via.ecp.fr>
2012-09-23 00:08:08 +0200

    Add support for geolocalisation via the phonenumbers lib in the get_cid_name.py script. If you add --geoloc, the numbers that are not found in OpenERP will be geolocalized.
    Thanks to Ludovic Gasc for making me discover this great "phonenumbers" lib.

--- asterisk_click2dial/scripts/get_cid_name_timeout.sh
+++ asterisk_click2dial/scripts/get_cid_name_timeout.sh
@@ -14,5 +14,12 @@
 # in the "coreutils" package
 
 # The first argument of the "timeout" command is the maximum execution time
-# In this example, we chose 1 second
-timeout 1s /usr/local/bin/get_cid_name.py -s openerp.mycompany.com -d erp_prod -u 12 -w "mypasswd"
+# In this example, we chose 2 seconds. Note that geolocalisation takes about
+# 1 second on an small machine ; so if you enable the --geoloc option,
+# don't put a 1 sec timeout !
+
+# To test this script manually (i.e. outside of Asterisk), run :
+# echo "agi_callerid:0141981242"|get_cid_name_timeout.sh
+# where 0141981242 is a phone number that could be presented by the calling party
+
+timeout 2s /usr/local/bin/get_cid_name.py --server openerp.mycompany.com --database erp_prod --user-id 12 --password "thepasswd" --geoloc --geoloc-country "FR" --geoloc-lang "fr"
