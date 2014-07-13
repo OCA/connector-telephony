65b7dc88c386762ed1376c74c0fbeb00b8310c09
Alexis de Lattre <alexis@via.ecp.fr>
2014-07-14 01:04:06 +0200

    Add monkey-patching of format function to add phone=True and phone_format='xxx' arguments.

--- base_phone/__openerp__.py
+++ base_phone/__openerp__.py
@@ -30,7 +30,19 @@
 Base Phone
 ==========
 
-This module validate phone numbers using the phonenumbers Python library, which is a port of the lib used in Android smartphones. It also adds tel: links on phone numbers.
+This module validate phone numbers using the *phonenumbers* Python library, which is a port of the library used in Android smartphones. For example, if your user is linked to a French company and you update the form view of a partner with a badly written French phone number such as '01-55-42-12-42', Odoo will automatically update the phone number to E.164 format '+33155421242' and display in the form view of the partner the readable equivalent '+33 1 55 42 12 42'.
+
+This module also adds *tel:* links on phone numbers and *fax:* links on fax numbers. If you have a softphone or a client software on your PC that is associated with *tel:* links, the softphone should propose you to dial the phone number when you click on such a link.
+
+This module also updates the format() function for reports and adds 2 arguments :
+
+* *phone* : should be True for a phone number, False (default) otherwize.
+* *phone_format* : it can have 3 possible values :
+    * *international* (default) : the report will display '+33 1 55 42 12 42'
+    * *national* : the report will display '01 55 42 12 42'
+    * *e164* : the report will display '+33155421242'
+
+For example, in the Sale Order report, to display the phone number of the Salesman, you can write :  o.user_id and o.user_id.phone and format(o.user_id.phone, phone=True, phone_format='national') or ''
 
 This module is independant from the Asterisk connector.
 
