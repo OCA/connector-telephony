a10e2a685ae5e3b7864697f75bab2be3bc9eab6d
Sandy Carter <sandy.carter@savoirfairelinux.com>
2014-11-05 14:38:06 -0500

    Fix code style

--- base_phone_popup/__openerp__.py
+++ base_phone_popup/__openerp__.py
@@ -30,21 +30,28 @@
 Base Phone Pop-up
 =================
 
-When the user receives a phone call, OpenERP can automatically open the corresponding partner/lead/employee/... in a pop-up without any action from the user.
+When the user receives a phone call, OpenERP can automatically open the
+corresponding partner/lead/employee/... in a pop-up without any action from the
+user.
 
 The module *web_action_request* can be downloaded with Mercurial:
 
 hg clone http://bitbucket.org/anybox/web_action_request
 
-It depends on 2 other modules, *web_longpolling* and *web_socketio*, that can be downloaded with this command:
+It depends on 2 other modules, *web_longpolling* and *web_socketio*, that can
+be downloaded with this command:
 
 hg clone http://bitbucket.org/anybox/web_socketio
 
-You will find some hints in this documentation : https://bitbucket.org/anybox/web_action_request
+You will find some hints in this documentation :
+https://bitbucket.org/anybox/web_action_request
 
-Warning : proxying WebSockets is only supported since Nginx 1.3.13 ; the feature provided by this module won't work with older versions of Nginx.
+Warning : proxying WebSockets is only supported since Nginx 1.3.13 ; the
+feature provided by this module won't work with older versions of Nginx.
 
-TODO : document this new feature on the Akretion Web site : http://www.akretion.com/en/products-and-services/openerp-asterisk-voip-connector """,
+TODO : document this new feature on the Akretion Web site :
+http://www.akretion.com/products-and-services/openerp-asterisk-voip-connector
+""",
     'author': 'Akretion',
     'website': 'http://www.akretion.com/',
     'depends': ['base_phone', 'web_action_request'],
