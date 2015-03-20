9658991bf8826dbdebdd5bd006c989ce1907c8d1
Alexis de Lattre <alexis@via.ecp.fr>
2015-03-20 01:00:38 +0100

    If you enter a phone number in national format, it is now interpreted from the country of the object (partner, lead, hr.applicant), not from the country of the user... a very wanted feature !

--- hr_phone/hr_phone.py
+++ hr_phone/hr_phone.py
@@ -26,16 +26,20 @@ from openerp.osv import orm
 class hr_employee(orm.Model):
     _name = 'hr.employee'
     _inherit = ['hr.employee', 'phone.common']
+    _phone_fields = ['work_phone', 'mobile_phone']
+    _phone_name_sequence = 30
+    _country_field = 'country_id'
+    _partner_field = None
 
     def create(self, cr, uid, vals, context=None):
         vals_reformated = self._generic_reformat_phonenumbers(
-            cr, uid, vals, context=context)
+            cr, uid, None, vals, context=context)
         return super(hr_employee, self).create(
             cr, uid, vals_reformated, context=context)
 
     def write(self, cr, uid, ids, vals, context=None):
         vals_reformated = self._generic_reformat_phonenumbers(
-            cr, uid, vals, context=context)
+            cr, uid, ids, vals, context=context)
         return super(hr_employee, self).write(
             cr, uid, ids, vals_reformated, context=context)
 
