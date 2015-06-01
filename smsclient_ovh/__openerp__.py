0485026f2f90e9c1b6297a62c3fd464c1d2b538b
Valentin Chemiere <valentin.chemiere@akretion.com>
2015-06-01 11:46:57 +0200

    Updated openerp.py

--- smsclient_ovh/__openerp__.py
+++ smsclient_ovh/__openerp__.py
@@ -33,23 +33,8 @@
     "description": """
 SMS Client module provides:
 -------------
-Sending SMSs very easily, individually or collectively.
 
-*Generalities
-
-OpenERP does not directly generate the SMS you will have to subscribe to an operator with a web interface (Type OVH) or via a URL generation.
-If you want to use a 'SMPP Method' you must have to install the library "Soap" which can be installed with: apt-get install python-soappy.
-You can find it on https://pypi.python.org/pypi/SOAPpy/
-You don't need it if you use a "HTTP Method' to send the SMS.
-
-*Use Multiple Gateways.
-
-The Gateway configuration is performed directly in the configuration menu. For each gateway, you have to fill in the information for your operator.
-
-To validate Gateway, code is send to a mobile phone, when received enter it to confirm SMS account.
-
-This Module was developped by SYLEAM and OpenERP SA in a first place.
-Then, it was updated to the 7.0 version by Julius Network Solutions.
+OVH http connector
     """,
     "website": "http://julius.fr",
     "category": "Tools",
