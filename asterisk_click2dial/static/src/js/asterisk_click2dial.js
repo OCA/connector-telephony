fd21e70f94091dbb72ca850c6845779835163d7a
Alexis de Lattre <alexis.delattre@akretion.com>
2016-05-19 16:34:46 +0200

    Port asterisk_click2dial to new API

--- asterisk_click2dial/static/src/js/asterisk_click2dial.js
+++ asterisk_click2dial/static/src/js/asterisk_click2dial.js
@@ -1,62 +1,62 @@
-/* Asterisk_click2dial module for OpenERP
-   Copyright (C) 2014 Alexis de Lattre <alexis@via.ecp.fr>
-   The licence is in the file __openerp__.py */
+/*  © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).   */
 
-openerp.asterisk_click2dial = function (instance) {
+odoo.define('asterisk_click2dial.OpenCaller', function (require) {
+"use strict";
 
-    var _t = instance.web._t;
+var _t = core._t;
+var Widget = require('web.Widget');
+var OpenCaller = Widget.extend({
+    template: 'asterisk_click2dial.OpenCaller',
 
-    instance.web.OpenCaller = instance.web.Widget.extend({
-        template:'asterisk_click2dial.OpenCaller',
+    start: function () {
+        this.$('#asterisk-open-caller').on(
+            'click', this.on_open_caller);
+        this._super();
+    },
 
-        start: function () {
-            this.$('#asterisk-open-caller').on(
-                'click', this.on_open_caller);
-            this._super();
-        },
+    on_open_caller: function (event) {
+        event.stopPropagation();
+        var self = this;
+        self.rpc('/asterisk_click2dial/get_record_from_my_channel', {}).done(function(r) {
+        // console.log('RESULT RPC r='+r);
+        // console.log('RESULT RPC type r='+typeof r);
+        if (r === false) {
+             self.do_notify(
+                _t('Failure'),
+                _t('Problem in the connection to Asterisk'));
+        }
+        else if (typeof r == 'string') {
+             var action = {
+                name: _t('Number Not Found'),
+                type: 'ir.actions.act_window',
+                res_model: 'number.not.found',
+                view_mode: 'form',
+                views: [[false, 'form']],
+                target: 'new',
+                context: {'default_calling_number': r},
+             };
+            instance.client.action_manager.do_action(action);
 
-        on_open_caller: function (event) {
-            event.stopPropagation();
-            var self = this;
-            self.rpc('/asterisk_click2dial/get_record_from_my_channel', {}).done(function(r) {
-            // console.log('RESULT RPC r='+r);
-            // console.log('RESULT RPC type r='+typeof r);
-            if (r === false) {
-                 self.do_notify(
-                    _t('Failure'),
-                    _t('Problem in the connection to Asterisk'));
             }
-            else if (typeof r == 'string') {
-                 var action = {
-                    name: _t('Number Not Found'),
-                    type: 'ir.actions.act_window',
-                    res_model: 'number.not.found',
-                    view_mode: 'form',
-                    views: [[false, 'form']],
-                    target: 'new',
-                    context: {'default_calling_number': r},
-                 };
-                instance.client.action_manager.do_action(action);
- 
-                }
-            else if (typeof r == 'object' && r.length == 3) {
-                self.do_notify( // Not working
-                    _t('Success'),
-                    _t('Moving to %s ID %d', r[0], r[1]));
-                var action = {
-                    type: 'ir.actions.act_window',
-                    res_model: r[0],
-                    res_id: r[1],
-                    view_mode: 'form,tree',
-                    views: [[false, 'form']],
-                    target: 'current',
-                    context: {},
-                };
-                instance.client.action_manager.do_action(action);
-            }
-        });
-       },
+        else if (typeof r == 'object' && r.length == 3) {
+            self.do_notify( // Not working
+                _t('Success'),
+                _t('Moving to %s ID %d', r[0], r[1]));
+            var action = {
+                type: 'ir.actions.act_window',
+                res_model: r[0],
+                res_id: r[1],
+                view_mode: 'form,tree',
+                views: [[false, 'form']],
+                target: 'current',
+                context: {},
+            };
+            instance.client.action_manager.do_action(action);
+        }
     });
+   },
+});
 
     instance.web.UserMenu.include({
         do_update: function(){
@@ -68,6 +68,6 @@ openerp.asterisk_click2dial = function (instance) {
         },
     });
 
-};
+});
 
 
