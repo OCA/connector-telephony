f6fe20fd305bd7781bcce1d73cf02d73941748b5
Alexis de Lattre <alexis.delattre@akretion.com>
2016-05-21 22:19:07 +0200

    Port event_phone and hr_recruitment_phone to v9

--- hr_recruitment_phone/hr_recruitment_phone.py
+++ hr_recruitment_phone/hr_recruitment_phone.py
@@ -1,29 +1,11 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    HR Recruitment phone module for Odoo/OpenERP
-#    Copyright (c) 2012-2014 Akretion (http://www.akretion.com)
-#    @author: Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2012-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.osv import orm
+from openerp import models, api
 
 
-class hr_applicant(orm.Model):
+class HrApplicant(models.Model):
     _name = 'hr.applicant'
     _inherit = ['hr.applicant', 'phone.common']
     _phone_fields = ['partner_phone', 'partner_mobile']
@@ -31,14 +13,12 @@ class hr_applicant(orm.Model):
     _country_field = None
     _partner_field = 'partner_id'
 
-    def create(self, cr, uid, vals, context=None):
-        vals_reformated = self._generic_reformat_phonenumbers(
-            cr, uid, None, vals, context=context)
-        return super(hr_applicant, self).create(
-            cr, uid, vals_reformated, context=context)
+    @api.model
+    def create(self, vals):
+        vals_reformated = self._reformat_phonenumbers_create(vals)
+        return super(HrApplicant, self).create(vals_reformated)
 
-    def write(self, cr, uid, ids, vals, context=None):
-        vals_reformated = self._generic_reformat_phonenumbers(
-            cr, uid, ids, vals, context=context)
-        return super(hr_applicant, self).write(
-            cr, uid, ids, vals_reformated, context=context)
+    @api.multi
+    def write(self, vals):
+        vals_reformated = self._reformat_phonenumbers_write(vals)
+        return super(HrApplicant, self).write(vals_reformated)
