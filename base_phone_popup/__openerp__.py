79e1634ca1bc2b218636b80b3b7046985b5ef93f
Alexis de Lattre <alexis.delattre@akretion.com>
2016-05-24 13:50:26 +0200

    Port base_phone_popup to v9

--- base_phone_popup/__openerp__.py
+++ base_phone_popup/__openerp__.py
@@ -1,28 +1,11 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Base Phone Pop-up module for Odoo/OpenERP
-#    Copyright (C) 2014 Alexis de Lattre <alexis@via.ecp.fr>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
     'name': 'Base Phone Pop-up',
-    'version': '8.0.0.4.0',
+    'version': '9.0.1.0.0',
     'category': 'Phone',
     'license': 'AGPL-3',
     'summary': 'Pop-up the related form view to the user on incoming calls',
@@ -55,10 +38,6 @@ http://www.akretion.com/products-and-services/openerp-asterisk-voip-connector
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'http://www.akretion.com/',
     'depends': ['base_phone', 'web_action_request'],
-    'data': [
-        'res_users_view.xml',
-        ],
-    'images': [],
-    'active': False,
-    'installable': False,
+    'data': ['res_users_view.xml'],
+    'installable': True,
 }
