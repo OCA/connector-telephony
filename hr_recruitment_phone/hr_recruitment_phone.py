7b9b06205f4ec48d21fe96ba53cfbbd3748f49fa
Alexis de Lattre <alexis.delattre@akretion.com>
2016-06-25 00:41:55 +0200

    Started to implement solution d) as discussed in https://github.com/OCA/connector-telephony/issues/90
    The revolution in the connector-telephony project has started !

--- hr_recruitment_phone/hr_recruitment_phone.py
+++ hr_recruitment_phone/hr_recruitment_phone.py
@@ -3,25 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, api
+from openerp.addons.base_phone.fields import Phone
 
 
 class HrApplicant(models.Model):
-    _name = 'hr.applicant'
-    _inherit = ['hr.applicant', 'phone.common']
-    _phone_fields = ['partner_phone', 'partner_mobile']
+    _inherit = 'hr.applicant'
     _phone_name_sequence = 50
-    _country_field = None
-    _partner_field = 'partner_id'
 
-    @api.model
-    def create(self, vals):
-        vals_reformated = self._reformat_phonenumbers_create(vals)
-        return super(HrApplicant, self).create(vals_reformated)
-
-    @api.multi
-    def write(self, vals):
-        vals_reformated = self._reformat_phonenumbers_write(vals)
-        return super(HrApplicant, self).write(vals_reformated)
+    partner_phone = Phone(partner_field='partner_id')
+    partner_mobile = Phone(partner_field='partner_id')
 
     @api.multi
     def name_get(self):
