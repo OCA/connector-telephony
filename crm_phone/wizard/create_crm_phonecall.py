798a0549459300932edcbe27436c32b9b1c92c3f
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-28 15:35:46 +0200

    try/except on import phonenumbers lib

--- crm_phone/wizard/create_crm_phonecall.py
+++ crm_phone/wizard/create_crm_phonecall.py
@@ -3,7 +3,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, api, _
-import phonenumbers
+import logging
+logger = logging.getLogger(__name__)
+
+try:
+    import phonenumbers
+except ImportError:
+    logger.debug('Cannot import phonenumbers lib.')
 
 
 class WizardCreateCrmPhonecall(models.TransientModel):
