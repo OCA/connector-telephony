26a17a54e5db303621a3a9f182c6f562c31bdb61
Trever L. Adams <trever.adams@gmail.com>
2017-06-13 11:02:53 -0600

    More PEP8 related cleanups.

--- freeswitch_click2dial/controller.py
+++ freeswitch_click2dial/controller.py
@@ -14,5 +14,5 @@ class FreeSWITCHClick2dialController(http.Controller):
         '/freeswitch_click2dial/get_record_from_my_channel/',
         type='json', auth='public')
     def get_record_from_my_channel(self, **kw):
-        FreeswitchServer = http.request.env['freeswitch.server']
-        return FreeswitchServer.get_record_from_my_channel()
+       FreeswitchServer = http.request.env['freeswitch.server']
+       return FreeswitchServer.get_record_from_my_channel()
