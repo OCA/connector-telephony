528370f32147b6318b3c28bc63230e1cf7c85a0e
Andhitia Rama <andhitia.r@gmail.com>
2017-07-26 21:10:03 +0700

    [BACKPORT][8.0] base_sms_client

--- base_sms_client/models/sms_sms.py
+++ base_sms_client/models/sms_sms.py
@@ -6,7 +6,7 @@
 # Copyright (C) 2015 Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, _
+from openerp import models, fields, api, _
 import logging
 _logger = logging.getLogger(__name__)
 
--- base_sms_client/wizard/mass_sms.py
+++ base_sms_client/wizard/mass_sms.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2015 Valentin Chemiere <valentin.chemiere@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from openerp import models, fields, api
 
 
 class WizardMassSms(models.TransientModel):
