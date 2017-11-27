bc98acc896b9998118e5d5043c9ca786989c0ae6
Istvan Szalai <istvan.szalai@savoirfairelinux.com>
2017-11-27 10:01:32 -0500

    [FIX] short headers

--- base_phone_validation/__manifest__.py
+++ base_phone_validation/__manifest__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2017 Savoir-faire Linux , Jordi Riera <kender.jr@gmail.com>
+# © 2015 Jordi Riera <kender.jr@gmail.com>
+# © 2017 Savoir-faire Linux
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
