40dec8393e78390e696b2b0dc7624503e4067b55
Alexis de Lattre <alexis.delattre@akretion.com>
2018-11-15 22:27:08 +0100

    Port to v12 base_phone, asterisk_click2dial, crm_phone, hr_phone, event_phone and hr_recruitment_phone

--- crm_phone/models/crm_lead.py
+++ crm_phone/models/crm_lead.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
-# © 2012-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2012-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api
-from odoo.addons.base_phone.fields import Phone, Fax
 
 
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
     _phone_name_sequence = 20
+    _phone_name_fields = ['phone', 'mobile']
 
-    phone = Phone(country_field='country_id', partner_field='partner_id')
-    mobile = Phone(country_field='country_id', partner_field='partner_id')
-    fax = Fax(country_field='country_id', partner_field='partner_id')
     phonecall_ids = fields.One2many(
         'crm.phonecall', 'opportunity_id', string='Phone Calls')
     phonecall_count = fields.Integer(
-        compute='_count_phonecalls', string='Number of Phonecalls',
+        compute='_compute_phonecall_count', string='Number of Phonecalls',
+        readonly=True)
+    partner_address_mobile = fields.Char(
+        string='Partner Contact Mobile', related='partner_id.mobile',
         readonly=True)
 
-    @api.multi
     def name_get(self):
         if self._context.get('callerid'):
             res = []
@@ -37,13 +37,11 @@ class CrmLead(models.Model):
         else:
             return super(CrmLead, self).name_get()
 
-    @api.multi
     @api.depends('phonecall_ids')
-    def _count_phonecalls(self):
-        cpo = self.env['crm.phonecall']
-        for lead in self:
-            try:
-                lead.phonecall_count = cpo.search_count(
-                    [('opportunity_id', '=', lead.id)])
-            except:
-                lead.phonecall_count = 0
+    def _compute_phonecall_count(self):
+        rg_res = self.env['crm.phonecall'].read_group(
+            [('opportunity_id', 'in', self.ids)],
+            ['opportunity_id'], ['opportunity_id'])
+        for rg_re in rg_res:
+            lead = self.browse(rg_re['opportunity_id'][0])
+            lead.phonecall_count = rg_re['opportunity_id_count']
--- crm_phone/models/res_users.py
+++ crm_phone/models/res_users.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2012-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2012-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
