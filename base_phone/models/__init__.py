badf8e809c8351041e7afa01a45fa42db7e07537
Alexis de Lattre <alexis.delattre@akretion.com>
2018-11-15 22:27:08 +0100

    Port to v12 base_phone, asterisk_click2dial, crm_phone, hr_phone, event_phone and hr_recruitment_phone

--- base_phone/models/__init__.py
+++ base_phone/models/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from . import base
+from . import phone_validation_mixin
 from . import res_company
-from . import base_config_settings
+from . import res_config_settings
 from . import res_partner
 from . import phone_common
-from . import ir_fields_converter
