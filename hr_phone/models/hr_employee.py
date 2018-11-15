2c3f047f7e0cfaebfa4f3725e5a17d5f570d66ad
Alexis de Lattre <alexis.delattre@akretion.com>
2018-11-15 22:27:08 +0100

    Port to v12 base_phone, asterisk_click2dial, crm_phone, hr_phone, event_phone and hr_recruitment_phone

--- hr_phone/models/hr_employee.py
+++ hr_phone/models/hr_employee.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
-# © 2012-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2012-2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
-from odoo.addons.base_phone.fields import Phone
+from odoo import api, models
 
 
 class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
+    _name = 'hr.employee'
+    _inherit = ['hr.employee', 'phone.validation.mixin']
     _phone_name_sequence = 30
+    _phone_name_fields = ['work_phone', 'mobile_phone']
 
-    work_phone = Phone(country_field='country_id')
-    mobile_phone = Phone(country_field='country_id')
+    @api.onchange('work_phone')
+    def work_phone_change(self):
+        if self.work_phone:
+            self.work_phone = self.phone_format(self.work_phone)
+
+    @api.onchange('mobile_phone')
+    def mobile_phone_change(self):
+        if self.mobile_phone:
+            self.mobile_phone = self.phone_format(self.mobile_phone)
