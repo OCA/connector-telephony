d7978eaf545a8922cb0b07e93825bb2848bf2822
Christophe Langenberg <christophe@langenberg.be>
2020-07-31 18:42:10 +0200

    [IMP] base_phone: black, isort, prettier

--- base_phone/__manifest__.py
+++ base_phone/__manifest__.py
@@ -1,40 +1,27 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-2018 Akretion France
 # @author: Alexis de Lattre <alexis@via.ecp.fr>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Base Phone',
-    'version': '12.0.1.0.1',
-    'category': 'Phone',
-    'license': 'AGPL-3',
-    'summary': 'Validate phone numbers',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com/',
-    'depends': ['phone_validation', 'base_setup'],
-    'external_dependencies': {'python': ['phonenumbers']},
-    'data': [
-        'security/phone_security.xml',
-        'security/ir.model.access.csv',
-        'views/res_config_settings.xml',
-        'views/res_users_view.xml',
-        'wizard/reformat_all_phonenumbers_view.xml',
-<<<<<<< HEAD
-<<<<<<< HEAD
-        'web_phone.xml',
-=======
-        'wizard/number_not_found_view.xml',
-        ],
-    'js': [
-        'static/src/js/*.js',
-        'static/lib/js/*.js',
->>>>>>> Feature "Open Calling Partner" replaced by "Open Caller", with a completely new behavior
-=======
-        'wizard/number_not_found_view.xml',
-        'views/web_phone.xml',
->>>>>>> Port to v12 base_phone, asterisk_click2dial, crm_phone, hr_phone, event_phone and hr_recruitment_phone
-        ],
-    'qweb': ['static/src/xml/phone.xml'],
-    'images': [],
-    'installable': True,
+    "name": "Base Phone",
+    "version": "13.0.1.0.1",
+    "category": "Phone",
+    "license": "AGPL-3",
+    "summary": "Validate phone numbers",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "http://www.akretion.com/",
+    "depends": ["phone_validation", "base_setup"],
+    "external_dependencies": {"python": ["phonenumbers"]},
+    "data": [
+        "security/phone_security.xml",
+        "security/ir.model.access.csv",
+        "views/res_config_settings.xml",
+        "views/res_users_view.xml",
+        "wizard/reformat_all_phonenumbers_view.xml",
+        "wizard/number_not_found_view.xml",
+        "views/web_phone.xml",
+    ],
+    "qweb": ["static/src/xml/phone.xml"],
+    "images": [],
+    "installable": True,
 }
--- base_phone/models/res_partner.py
+++ base_phone/models/res_partner.py
@@ -7,20 +7,20 @@ from odoo import api, models
 
 
 class ResPartner(models.Model):
-    _name = 'res.partner'
+    _name = "res.partner"
     # inherit on phone.validation.mixin (same as in crm_phone_validation,
     # but base_phone only depends on phone_validation,
     # not on crm_phone_validation)
-    _inherit = ['res.partner', 'phone.validation.mixin']
+    _inherit = ["res.partner", "phone.validation.mixin"]
     _phone_name_sequence = 10
-    _phone_name_fields = ['phone', 'mobile']
+    _phone_name_fields = ["phone", "mobile"]
 
     def name_get(self):
-        if self._context.get('callerid'):
+        if self._context.get("callerid"):
             res = []
             for partner in self:
                 if partner.parent_id and partner.parent_id.is_company:
-                    name = u'%s (%s)' % (partner.name, partner.parent_id.name)
+                    name = u"{} ({})".format(partner.name, partner.parent_id.name)
                 else:
                     name = partner.name
                 res.append((partner.id, name))
@@ -33,12 +33,12 @@ class ResPartner(models.Model):
     # because we don't want base_phone to depend on crm.
     # When both base_phone AND crm_phone_validation are installed,
     # as the methods have the same name, only one of the 2 will be executed
-    @api.onchange('phone', 'country_id', 'company_id')
+    @api.onchange("phone", "country_id", "company_id")
     def _onchange_phone_validation(self):
         if self.phone:
             self.phone = self.phone_format(self.phone)
 
-    @api.onchange('mobile', 'country_id', 'company_id')
+    @api.onchange("mobile", "country_id", "company_id")
     def _onchange_mobile_validation(self):
         if self.mobile:
             self.mobile = self.phone_format(self.mobile)
