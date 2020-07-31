a002c8d5c458e708f8bf294abecfb3bc1acfeb61
Christophe Langenberg <christophe@langenberg.be>
2020-07-31 18:42:10 +0200

    [IMP] base_phone: black, isort, prettier

--- base_phone/models/phone_validation_mixin.py
+++ base_phone/models/phone_validation_mixin.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Akretion France
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -8,14 +7,11 @@ from odoo import models
 
 
 class PhoneValidationMixin(models.AbstractModel):
-    _inherit = 'phone.validation.mixin'
+    _inherit = "phone.validation.mixin"
 
     def _phone_get_country(self):
-        if 'country_id' in self and self.country_id:
+        if "country_id" in self and self.country_id:
             return self.country_id
-        if (
-                'partner_id' in self and
-                self.partner_id and
-                self.partner_id.country_id):
+        if "partner_id" in self and self.partner_id and self.partner_id.country_id:
             return self.partner_id.country_id
         return self.env.user.company_id.country_id
--- base_phone/static/src/js/phone_widget.js
+++ base_phone/static/src/js/phone_widget.js
@@ -3,28 +3,26 @@
    @author: Alexis de Lattre <alexis.delattre@akretion.com>
    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
-odoo.define('base_phone.updatedphone_widget', function (require) {
+odoo.define("base_phone.updatedphone_widget", function(require) {
     "use strict";
 
-
-    var core = require('web.core');
-    var FieldPhone = require('web.basic_fields').FieldPhone;
+    var core = require("web.core");
+    var FieldPhone = require("web.basic_fields").FieldPhone;
     var _t = core._t;
 
     FieldPhone.include({
-
         /* Always enable phone link tel:, not only on small screens  */
-        _canCall: function () {
+        _canCall: function() {
             return true;
         },
-        showDialButton: function () {
-            // must be inherited by ipbx specific modules
+        showDialButton: function() {
+            // Must be inherited by ipbx specific modules
             // and set to true
             return false;
         },
 
         _renderReadonly: function() {
-            // create a link to trigger action on server
+            // Create a link to trigger action on server
             // this link will be after the <a href="tel:">
             this._super();
 
@@ -33,16 +31,18 @@ odoo.define('base_phone.updatedphone_widget', function (require) {
             }
             var self = this;
 
-            // create our link
-            var dial = $('<a href="#" class="dial"><div class="label label-primary">☎ Dial</div></a>');
+            // Create our link
+            var dial = $(
+                '<a href="#" class="dial"><div class="label label-primary">☎ Dial</div></a>'
+            );
 
-            // add a parent element
+            // Add a parent element
             // it's not possible to append to $el directly
             // because $el don't have any parent yet
-            var parent = $('<div>');
-            parent.append([this.$el[0], ' ', dial]);
+            var parent = $("<div>");
+            parent.append([this.$el[0], " ", dial]);
 
-            // replace this.$el by our new container
+            // Replace this.$el by our new container
             this.$el = parent;
 
             var phone_num = this.value;
@@ -53,48 +53,54 @@ odoo.define('base_phone.updatedphone_widget', function (require) {
         click2dial: function(phone_num) {
             var self = this;
             this.do_notify(
-                _.str.sprintf(_t('Click2dial to %s'), phone_num),
-                _t('Unhook your ringing phone'),
-                false);
+                _.str.sprintf(_t("Click2dial to %s"), phone_num),
+                _t("Unhook your ringing phone"),
+                false
+            );
             var params = {
-                'phone_number': phone_num,
-                'click2dial_model': this.model,
-                'click2dial_id': this.res_id};
+                phone_number: phone_num,
+                click2dial_model: this.model,
+                click2dial_id: this.res_id,
+            };
             return this._rpc({
-                model: 'phone.common',
+                model: "phone.common",
                 context: params,
-                method: 'click2dial',
-                args: [phone_num]
-            }).then(function(r) {
-                console.log('successfull', r);
-                if (r === false) {
-                    self.do_warn("Click2dial failed");
-                } else if (typeof r === 'object') {
-                    self.do_notify(
-                            _t('Click2dial successfull'),
-                            _.str.sprintf(_t('Number dialed: %s'), r.dialed_number),
-                            false);
-                    if (r.action_model) {
-                        var action = {
-                            name: r.action_name,
-                            type: 'ir.actions.act_window',
-                            res_model: r.action_model,
-                            view_mode: 'form',
-                            views: [[false, 'form']],
-                            target: 'new',
-                            context: params,
-                        };
-                        return self.do_action(action);
+                method: "click2dial",
+                args: [phone_num],
+            }).then(
+                function(r) {
+                    console.log("successfull", r);
+                    if (r === false) {
+                        self.do_warn("Click2dial failed");
+                    } else if (typeof r === "object") {
+                        self.do_notify(
+                            _t("Click2dial successfull"),
+                            _.str.sprintf(_t("Number dialed: %s"), r.dialed_number),
+                            false
+                        );
+                        if (r.action_model) {
+                            var action = {
+                                name: r.action_name,
+                                type: "ir.actions.act_window",
+                                res_model: r.action_model,
+                                view_mode: "form",
+                                views: [[false, "form"]],
+                                target: "new",
+                                context: params,
+                            };
+                            return self.do_action(action);
+                        }
                     }
+                },
+                function(r) {
+                    console.log("on error");
+                    self.do_warn("Click2dial failed");
                 }
-            }, function (r) {
-                console.log('on error');
-                self.do_warn("Click2dial failed");
-            });
-        }
+            );
+        },
     });
 
     return {
-        'FieldPhone': FieldPhone
+        FieldPhone: FieldPhone,
     };
 });
