b713df51fc97c9bd27d5af30888a8972c4bfe93f
Christophe Langenberg <christophe@langenberg.be>
2020-07-31 18:42:10 +0200

    [IMP] base_phone: black, isort, prettier

--- base_phone/models/base.py
+++ base_phone/models/base.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
 # © 2016 SYLEAM
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo import api, models
+
 from .. import common
-from odoo import models, api
 
 
 class Base(models.AbstractModel):
-    _inherit = 'base'
+    _inherit = "base"
 
     @api.multi
     def write(self, vals):
@@ -15,17 +15,17 @@ class Base(models.AbstractModel):
         if fields_to_convert:
             for record in self:
                 loc_vals = common.convert_all_phone_fields(
-                    record, vals, fields_to_convert)
+                    record, vals, fields_to_convert
+                )
                 super(Base, record).write(loc_vals)
             return True
         else:
             return super(Base, self).write(vals)
 
     @api.model
-    @api.returns('self', lambda value: value.id)
+    @api.returns("self", lambda value: value.id)
     def create(self, vals):
         fields_to_convert = common.get_phone_fields(self, vals)
         if fields_to_convert:
-            vals = common.convert_all_phone_fields(
-                self, vals, fields_to_convert)
+            vals = common.convert_all_phone_fields(self, vals, fields_to_convert)
         return super(Base, self).create(vals)
