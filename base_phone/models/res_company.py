2d4d2adcf430ffe813f03c24166a80dcce710fd5
Christophe Langenberg <christophe@langenberg.be>
2020-07-31 18:42:10 +0200

    [IMP] base_phone: black, isort, prettier

--- base_phone/models/res_company.py
+++ base_phone/models/res_company.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2018 Akretion France
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     number_of_digits_to_match_from_end = fields.Integer(
-        string='Number of Digits To Match From End',
+        string="Number of Digits To Match From End",
         default=8,
         help="In several situations, Odoo will have to find a "
         "Partner/Lead/Employee/... from a phone number presented by the "
@@ -20,10 +19,14 @@ class ResCompany(models.Model):
         "in Odoo is to try to match the end of the phone number in "
         "Odoo with the N last digits of the phone number presented "
         "by the calling party. N is the value you should enter in this "
-        "field.")
+        "field.",
+    )
 
-    _sql_constraints = [(
-        'number_of_digits_to_match_from_end_positive',
-        'CHECK (number_of_digits_to_match_from_end > 0)',
-        "The value of the field 'Number of Digits To Match From End' must "
-        "be positive.")]
+    _sql_constraints = [
+        (
+            "number_of_digits_to_match_from_end_positive",
+            "CHECK (number_of_digits_to_match_from_end > 0)",
+            "The value of the field 'Number of Digits To Match From End' must "
+            "be positive.",
+        )
+    ]
