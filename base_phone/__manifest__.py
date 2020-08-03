f0c7947b2ed55e542b716a4bf29d690ee59741d7
Christophe Langenberg <christophe@langenberg.be>
2020-08-03 12:47:57 +0200

    modified the manifest it's 'external_dependencies' again

--- base_phone/__manifest__.py
+++ base_phone/__manifest__.py
@@ -12,11 +12,7 @@
     "author": "Akretion,Odoo Community Association (OCA)",
     "website": "http://www.akretion.com/",
     "depends": ["phone_validation", "base_setup"],
-    "external_dependencies": {
-        "python": {
-            'phonenumbers': 'phonenumbers>=8.12.7'
-        }
-    },
+    "external_dependencies": {"python": ['phonenumbers']},
     "data": [
         "security/phone_security.xml",
         "security/ir.model.access.csv",
