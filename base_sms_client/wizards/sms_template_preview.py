24dad2beafd75cf91d1101b1a8ee670aaa59f25d
sbejaoui <souheil.bejaoui@acsone.eu>
2020-08-20 11:16:58 +0200

    [FIX] - fix sms preview wizard

--- base_sms_client/wizards/sms_template_preview.py
+++ base_sms_client/wizards/sms_template_preview.py
@@ -11,15 +11,28 @@ class SmsTemplatePreview(models.TransientModel):
     _description = "SMS Template Preview Wizard"
 
     @api.onchange('res_id')
-    @api.multi
     def on_change_res_id(self):
         if not self.res_id:
             return {}
         mail_values = {}
         if self._context.get('template_id'):
             template = self.env['sms.template'].browse(
-                self._context['template_id'])
+                self._context['template_id']
+            )
             self.name = template.name
             mail_values = template.generate_sms(self.res_id)
         for field in ['mobile', 'message', 'partner_ids']:
-            setattr(self, field, mail_values.get(field, False))
+            setattr(
+                self, field, mail_values.get(self.res_id).get(field, False)
+            )
+
+    @api.model
+    def default_get(self, fields):
+        result = super().default_get(fields)
+        if self._context.get('template_id'):
+            result['model_id'] = (
+                self.env['sms.template']
+                .browse(self._context['template_id'])
+                .model_id.id
+            )
+        return result
