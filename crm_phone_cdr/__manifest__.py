77c2a791b6aba9a4673d832da70977df4ea18c37
Chanakya Soni <chanakya.soni@serpentcs.com>
2020-12-30 21:19:33 +0530

    [Add] crm_phone_cdr

---
+++ crm_phone_cdr/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Base Phone CDR CRM",
+    'category': "CRM",
+    'summary' :" This module will add menu in CRM | Reports Menu",
+    'version': "12.0.1.0.1",
+    'depends': ['base_phone_cdr','crm'],
+    'data': [
+        'view/crm_view.xml'
+    ],
+    'license': 'AGPL-3',
+}
