7a306837e6496aa2d176d39bee3f44ade96cd5c9
Chanakya Soni <chanakya.soni@serpentcs.com>
2021-01-06 14:45:54 +0530

    Fixed Travis

--- base_phone_cdr/__manifest__.py
+++ base_phone_cdr/__manifest__.py
@@ -1,16 +1,17 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
-    'name': "Base Phone CDR",
-    'category': "web",
-    'version': "12.0.1.0.1",
-    'depends': ['base_phone_popup', 'web_notify'],
-    'data': [
-        'view/phone_cdr_view.xml',
-        'security/ir.model.access.csv',
-        'view/res_users_view.xml',
-        'view/res_partner_view.xml'
+    "name": "Base Phone CDR",
+    "category": "web",
+    "version": "12.0.1.0.0",
+    "author": "Serpent Consulting Services Pvt. Ltd., Odoo Community Association (OCA)",
+    "depends": ["base_phone_popup", "web_notify"],
+    "data": [
+        "view/phone_cdr_view.xml",
+        "security/ir.model.access.csv",
+        "view/res_users_view.xml",
+        "view/res_partner_view.xml",
     ],
-    'qweb': [
-        'static/src/xml/widget.xml',
-    ],
-    'license': 'AGPL-3',
+    "qweb": ["static/src/xml/widget.xml"],
+    "license": "AGPL-3",
 }
--- base_phone_cdr/models/res_partner.py
+++ base_phone_cdr/models/res_partner.py
@@ -1,28 +1,26 @@
-# -*- coding: utf-8 -*-
-
 from ast import literal_eval
 
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    total_cdr_count = fields.Integer(compute="_compute_total_cdr_count", 
-                                     string="Total CDR")
+    total_cdr_count = fields.Integer(
+        compute="_compute_total_cdr_count", string="Total CDR"
+    )
 
     def _compute_total_cdr_count(self):
         for partner in self:
-            cdr_records = self.env['phone.cdr'].search([
-                ('partner_id', '=', partner.id)
-            ])
+            cdr_records = self.env["phone.cdr"].search(
+                [("partner_id", "=", partner.id)]
+            )
             partner.total_cdr_count = cdr_records and len(cdr_records) or 0
-
 
     @api.multi
     def action_view_partner_cdr_records(self):
         self.ensure_one()
-        action = self.env.ref('base_phone_cdr.phone_cdr_view_action').read()[0]
-        action['domain'] = literal_eval(action['domain'])
-        action['domain'].append(('partner_id', '=', self.id))
+        action = self.env.ref("base_phone_cdr.phone_cdr_view_action").read()[0]
+        action["domain"] = literal_eval(action["domain"])
+        action["domain"].append(("partner_id", "=", self.id))
         return action
