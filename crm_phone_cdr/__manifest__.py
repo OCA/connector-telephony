15a9497d3de40fa0eeb37266a0410abd40245698
Chanakya Soni <chanakya.soni@serpentcs.com>
2021-01-06 14:57:16 +0530

    [Fix] Fix Travis : crm_phone_cdr

--- crm_phone_cdr/__manifest__.py
+++ crm_phone_cdr/__manifest__.py
@@ -1,11 +1,10 @@
 {
-    'name': "Base Phone CDR CRM",
-    'category': "CRM",
-    'summary' :" This module will add menu in CRM | Reports Menu",
-    'version': "12.0.1.0.1",
-    'depends': ['base_phone_cdr','crm'],
-    'data': [
-        'view/crm_view.xml'
-    ],
-    'license': 'AGPL-3',
+    "name": "Base Phone CDR CRM",
+    "category": "CRM",
+    "author": "Serpent Consulting Services Pvt. Ltd., Odoo Community Association (OCA)",
+    "summary": " This module will add menu in CRM | Reports Menu",
+    "version": "12.0.1.0.0",
+    "depends": ["base_phone_cdr", "crm"],
+    "data": ["view/crm_view.xml"],
+    "license": "AGPL-3",
 }
