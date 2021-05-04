0f5414790dea9ed3ff20fadb7418c4ca14bf30df
Pierrick Brun <pierrick.brun@akretion.com>
2021-05-04 10:18:26 +0200

    [MIG] base_phone: Migration to 14.0

--- base_phone/__manifest__.py
+++ base_phone/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Base Phone",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Phone",
     "license": "AGPL-3",
     "summary": "Validate phone numbers",
