b19d3f278211c257fa319e34955567162fd83d64
Pierrick Brun <pierrick.brun@akretion.com>
2021-05-04 10:18:26 +0200

    [MIG] base_phone: Migration to 14.0

--- base_phone/wizard/reformat_all_phonenumbers.py
+++ base_phone/wizard/reformat_all_phonenumbers.py
@@ -69,8 +69,8 @@ class ReformatAllPhonenumbers(models.TransientModel):
             }
         )
         logger.info("End of the phone number reformatting wizard")
-        action = self.env["ir.actions.act_window"].for_xml_id(
-            "base_phone", "reformat_all_phonenumbers_action"
+        action = (
+            self.env.ref("base_phone.reformat_all_phonenumbers_action").sudo().read()[0]
         )
         action["res_id"] = self.id
         return action
