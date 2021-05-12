b08119edea48acae80c2807e0c3652240bb434fb
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-13 00:55:37 +0200

    crm_phone: Migrate to v14
    Convert README to new subdir format

--- crm_phone/__manifest__.py
+++ crm_phone/__manifest__.py
@@ -1,39 +1,26 @@
-# -*- coding: utf-8 -*-
-# Copyright 2014-2018 Akretion France
+# Copyright 2014-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'CRM Phone',
-    'version': '12.0.1.0.1',
+    'version': '14.0.1.0.0',
     'category': 'Phone',
     'license': 'AGPL-3',
-    'summary': 'Validate phone numbers in CRM',
-    'description': """
-CRM Phone
-=========
-
-This module validate phone numbers in the CRM module, just like the
-*base_phone* module valide phone numbers in the Partner form. Please refer to
-the description of the *base_phone* module for more information.
-
-This module is independant from the Asterisk connector.
-
-Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com>
-for any help or question about this module.
-""",
+    'summary': 'Improve phone support in CRM',
     'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com/',
-    'depends': ['base_phone', 'crm_phone_validation'],
+    'maintainers': ['alexis-via'],
+    'website': 'https://github.com/OCA/connector-telephony',
+    'depends': ['base_phone', 'crm'],
     'external_dependencies': {'python': ['phonenumbers']},
     'conflicts': ['crm_voip'],
     'data': [
         'security/phonecall_security.xml',
         'security/ir.model.access.csv',
-        'view/crm_phonecall.xml',
-        'view/crm_lead.xml',
-        'view/res_partner.xml',
-        'view/res_users.xml',
+        'views/crm_phonecall.xml',
+        'views/crm_lead.xml',
+        'views/res_partner.xml',
+        'views/res_users.xml',
         'wizard/number_not_found_view.xml',
         'wizard/create_crm_phonecall_view.xml',
         ],
--- crm_phone/models/res_users.py
+++ crm_phone/models/res_users.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2012-2018 Akretion France
+# Copyright 2012-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -11,7 +10,7 @@ class ResUsers(models.Model):
 
     # Field name starts with 'context_' to allow modification by the user
     # in his preferences, cf odoo/odoo/addons/base/res/res_users.py
-    # in "def write()" of "class res_users(osv.osv)"
+    # in "def write()" of "class Users(models.Model)"
     context_propose_creation_crm_call = fields.Boolean(
         string='Propose to create a call in CRM after a click2dial',
         default=True)
