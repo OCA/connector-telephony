cec7d150deb908a095adaa59a633c1c31f3d8e61
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-12 23:15:01 +0200

    bsae_phone: FIX method get_record_from_phone_number()
    Migrate README to new subdir format
    Large code clean-up
    Remove dead lines
    self.env.user.company_id => self.env.company

--- base_phone/__manifest__.py
+++ base_phone/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2014-2018 Akretion France
+# Copyright 2014-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis@via.ecp.fr>
 # @migration from 12.0 to 13.0: Christophe Langenberg <Christophe@Langenberg.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -10,19 +10,19 @@
     "license": "AGPL-3",
     "summary": "Validate phone numbers",
     "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
     "website": "https://github.com/OCA/connector-telephony",
     "depends": ["phone_validation", "base_setup"],
     "external_dependencies": {"python": ["phonenumbers"]},
     "data": [
         "security/phone_security.xml",
         "security/ir.model.access.csv",
-        "views/res_config_settings.xml",
+        "wizard/res_config_settings.xml",
         "views/res_users_view.xml",
         "wizard/reformat_all_phonenumbers_view.xml",
         "wizard/number_not_found_view.xml",
         "views/web_phone.xml",
     ],
     "qweb": ["static/src/xml/phone.xml"],
-    "images": [],
     "installable": True,
 }
--- base_phone/wizard/res_config_settings.py
+++ base_phone/wizard/res_config_settings.py
@@ -1,4 +1,5 @@
-# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2017-2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
