77ef8750befeeb657216764eb86fb5ee0e333eaf
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-13 00:55:37 +0200

    crm_phone: Migrate to v14
    Convert README to new subdir format

--- crm_phone/models/crm_lead.py
+++ crm_phone/models/crm_lead.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2012-2018 Akretion France
+# Copyright 2012-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -14,18 +13,14 @@ class CrmLead(models.Model):
     phonecall_ids = fields.One2many(
         'crm.phonecall', 'opportunity_id', string='Phone Calls')
     phonecall_count = fields.Integer(
-        compute='_compute_phonecall_count', string='Number of Phonecalls',
-        readonly=True)
-    partner_address_mobile = fields.Char(
-        string='Partner Contact Mobile', related='partner_id.mobile',
-        readonly=True)
+        compute='_compute_phonecall_count', string='Number of Phonecalls')
 
     def name_get(self):
         if self._context.get('callerid'):
             res = []
             for lead in self:
                 if lead.partner_name and lead.contact_name:
-                    name = u'%s (%s)' % (lead.contact_name, lead.partner_name)
+                    name = '%s (%s)' % (lead.contact_name, lead.partner_name)
                 elif lead.partner_name:
                     name = lead.partner_name
                 elif lead.contact_name:
@@ -42,6 +37,6 @@ class CrmLead(models.Model):
         rg_res = self.env['crm.phonecall'].read_group(
             [('opportunity_id', 'in', self.ids)],
             ['opportunity_id'], ['opportunity_id'])
-        for rg_re in rg_res:
-            lead = self.browse(rg_re['opportunity_id'][0])
-            lead.phonecall_count = rg_re['opportunity_id_count']
+        mapped_data = dict([(x['opportunity_id'][0], x['opportunity_id_count']) for x in rg_res])
+        for lead in self:
+            lead.phonecall_count = mapped_data.get(lead.id, 0)
--- crm_phone/wizard/__init__.py
+++ crm_phone/wizard/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from . import number_not_found
 from . import create_crm_phonecall
