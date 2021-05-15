d4106597a817343ac7c495951a3f0f44e00c9fc0
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-15 17:57:08 +0200

    hr_recruitment_phone: black, isort, ...

--- hr_recruitment_phone/__manifest__.py
+++ hr_recruitment_phone/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'HR Recruitment Phone',
-    'version': '14.0.1.0.0',
-    'category': 'Phone',
-    'license': 'AGPL-3',
-    'summary': 'Validate phone numbers in HR Recruitment',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'maintainers': ['alexis-via'],
-    'website': 'https://github.com/OCA/connector-telephony',
-    'depends': ['base_phone', 'hr_recruitment'],
-    'data': [
-        'security/ir.model.access.csv',
-        ],
-    'installable': True,
-    'auto_install': True,
+    "name": "HR Recruitment Phone",
+    "version": "14.0.1.0.0",
+    "category": "Phone",
+    "license": "AGPL-3",
+    "summary": "Validate phone numbers in HR Recruitment",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/connector-telephony",
+    "depends": ["base_phone", "hr_recruitment"],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "auto_install": True,
 }
--- hr_recruitment_phone/models/hr_applicant.py
+++ hr_recruitment_phone/models/hr_applicant.py
@@ -6,29 +6,29 @@ from odoo import api, models
 
 
 class HrApplicant(models.Model):
-    _name = 'hr.applicant'
-    _inherit = ['hr.applicant', 'phone.validation.mixin']
+    _name = "hr.applicant"
+    _inherit = ["hr.applicant", "phone.validation.mixin"]
     _phone_name_sequence = 50
-    _phone_name_fields = ['partner_phone', 'partner_mobile']
+    _phone_name_fields = ["partner_phone", "partner_mobile"]
 
-    @api.onchange('partner_phone')
+    @api.onchange("partner_phone")
     def partner_phone_change(self):
         if self.partner_phone:
             self.partner_phone = self.phone_format(self.partner_phone)
 
-    @api.onchange('partner_mobile')
+    @api.onchange("partner_mobile")
     def partner_mobile_change(self):
         if self.partner_mobile:
             self.partner_mobile = self.phone_format(self.partner_mobile)
 
     def name_get(self):
-        if self._context.get('callerid'):
+        if self._context.get("callerid"):
             res = []
             for appl in self:
                 if appl.partner_id:
-                    name = '%s (%s)' % (appl.partner_id.name, appl.name)
+                    name = "%s (%s)" % (appl.partner_id.name, appl.name)
                 elif appl.partner_name:
-                    name = '%s (%s)' % (appl.partner_name, appl.name)
+                    name = "%s (%s)" % (appl.partner_name, appl.name)
                 else:
                     name = appl.name
                 res.append((appl.id, name))
