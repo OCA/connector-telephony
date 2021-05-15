b7f903b621ec730e16247a399c3f9f9712e31160
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-15 17:18:43 +0200

    hr_phone: black, isort, ...

--- hr_phone/__manifest__.py
+++ hr_phone/__manifest__.py
@@ -3,16 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'HR Phone',
-    'version': '14.0.1.0.0',
-    'category': 'Phone',
-    'license': 'AGPL-3',
-    'summary': 'Validate phone numbers in HR',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'maintainers': ['alexis-via'],
-    'website': 'https://github.com/OCA/connector-telephony',
-    'depends': ['base_phone', 'hr'],
-    'data': ['security/ir.model.access.csv'],
-    'installable': True,
-    'auto_install': True,
+    "name": "HR Phone",
+    "version": "14.0.1.0.0",
+    "category": "Phone",
+    "license": "AGPL-3",
+    "summary": "Validate phone numbers in HR",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/connector-telephony",
+    "depends": ["base_phone", "hr"],
+    "data": ["security/ir.model.access.csv"],
+    "installable": True,
+    "auto_install": True,
 }
