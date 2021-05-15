9ce1b2eff7256c28517668c3d7d74f2978a91f44
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-15 17:18:43 +0200

    hr_phone: black, isort, ...

--- hr_phone/models/hr_employee.py
+++ hr_phone/models/hr_employee.py
@@ -6,20 +6,20 @@ from odoo import api, models
 
 
 class HrEmployeePrivate(models.Model):
-    _name = 'hr.employee'
-    _inherit = ['hr.employee', 'phone.validation.mixin']
+    _name = "hr.employee"
+    _inherit = ["hr.employee", "phone.validation.mixin"]
     _phone_name_sequence = 30
-    _phone_name_fields = ['mobile_phone']
+    _phone_name_fields = ["mobile_phone"]
     # work_phone is now a computed field that take the value address_id.phone
     # Don't put emergency_phone in _phone_name_fields because it is not a phone
     # number of the employee
 
-    @api.onchange('mobile_phone')
+    @api.onchange("mobile_phone")
     def mobile_phone_change(self):
         if self.mobile_phone:
             self.mobile_phone = self.phone_format(self.mobile_phone)
 
-    @api.onchange('emergency_phone')
+    @api.onchange("emergency_phone")
     def emergency_phone_change(self):
         if self.emergency_phone:
             self.emergency_phone = self.phone_format(self.emergency_phone)
