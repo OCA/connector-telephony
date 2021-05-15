77bf4a1fa1fb82ffc39f0020dfc70e097e3f2678
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-15 17:36:46 +0200

    [MIG] event_phone from v12 to v14
    Migrate README to the new subdir format

--- event_phone/__manifest__.py
+++ event_phone/__manifest__.py
@@ -1,30 +1,17 @@
-# -*- coding: utf-8 -*-
-# Copyright 2014-2018 Akretion France
+# Copyright 2014-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
     'name': 'Event Phone',
-    'version': '12.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Phone',
     'license': 'AGPL-3',
     'summary': 'Validate phone numbers in Events',
-    'description': """
-Event Phone
-===========
-
-This module validate phone numbers in the Event module, just like the
-*base_phone* module valide phone numbers in the Partner form. Please refer to
-the description of the *base_phone* module for more information.
-
-This module is independant from the Asterisk connector.
-
-Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com>
-for any help or question about this module.
-""",
     'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com/',
+    'maintainers': ['alexis-via'],
+    'website': 'https://github.com/OCA/connector-telephony',
     'depends': ['base_phone', 'event'],
     'data': [
         'security/ir.model.access.csv',
--- event_phone/models/event_registration.py
+++ event_phone/models/event_registration.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2012-2018 Akretion France
+# Copyright 2012-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -10,9 +9,14 @@ class EventRegistration(models.Model):
     _name = 'event.registration'
     _inherit = ['event.registration', 'phone.validation.mixin']
     _phone_name_sequence = 100
-    _phone_name_fields = ['phone']
+    _phone_name_fields = ['phone', 'mobile']
 
     @api.onchange('phone')
     def phone_change(self):
         if self.phone:
             self.phone = self.phone_format(self.phone)
+
+    @api.onchange('mobile')
+    def mobile_change(self):
+        if self.mobile:
+            self.mobile = self.phone_format(self.mobile)
