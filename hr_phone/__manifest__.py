a58d7577297d9e019f4d8ab482db6952826d46e1
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-15 17:17:14 +0200

    [MIG] hr_phone from v12 to v14
    Migrate README to the new subdir format

--- hr_phone/__manifest__.py
+++ hr_phone/__manifest__.py
@@ -1,29 +1,16 @@
-# -*- coding: utf-8 -*-
-# Copyright 2014-2016 Akretion France
+# Copyright 2014-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'HR Phone',
-    'version': '12.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Phone',
     'license': 'AGPL-3',
     'summary': 'Validate phone numbers in HR',
-    'description': """
-HR Phone
-========
-
-This module validate phone numbers in the HR module, just like the *base_phone*
-module valide phone numbers in the Partner form. Please refer to the
-description of the *base_phone* module for more information.
-
-This module is independant from the Asterisk connector.
-
-Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com>
-for any help or question about this module.
-""",
     'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com/',
+    'maintainers': ['alexis-via'],
+    'website': 'https://github.com/OCA/connector-telephony',
     'depends': ['base_phone', 'hr'],
     'data': ['security/ir.model.access.csv'],
     'installable': True,
