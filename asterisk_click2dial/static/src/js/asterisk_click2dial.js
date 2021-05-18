dc73ec23d446b4acf47c14607503c69cd84506ff
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-19 00:34:26 +0200

    [MIG] asterisk_click2dial from v12 to v14
    PJSIP is now the default channel type instead of SIP
    Convert README to the new subdir format
    Minor code improvements

--- asterisk_click2dial/static/src/js/asterisk_click2dial.js
+++ asterisk_click2dial/static/src/js/asterisk_click2dial.js
@@ -1,5 +1,5 @@
-/*  Copyright 2014-2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-    Copyright 2015-2018 Juris Malinens (port to v9)
+/*  Copyright 2014-2021 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+    Copyright 2015-2021 Juris Malinens (port to v9)
     License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).   */
 
 odoo.define('asterisk_click2dial.systray.OpenCaller', function (require) {
