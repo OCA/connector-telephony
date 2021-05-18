77f68489dc333647cd2398742850a6f7168b045f
Alexis de Lattre <alexis.delattre@akretion.com>
2021-05-19 00:34:26 +0200

    [MIG] asterisk_click2dial from v12 to v14
    PJSIP is now the default channel type instead of SIP
    Convert README to the new subdir format
    Minor code improvements

--- asterisk_click2dial/controller.py
+++ asterisk_click2dial/controller.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2014-2018 Akretion France
+# Copyright 2014-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # Copyright 2015-2018 Juris Malinens (port to v9)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
