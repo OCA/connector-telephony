d5f42a6293535519de6e77e588495f117725e198
Kévin Roche <kevin.roche@akretion.com>
2021-06-21 08:55:16 +0200

    [ADD] 14.0_sms_no_automatic_delete

---
+++ sms_no_automatic_delete/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Akretion (http://www.akretion.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "No automatic deletion of SMS",
+    "summary": "Avoid automatic delete of sended sms",
+    "author": "Akretion",
+    "website": "https://github.com/OCA/connector-telephony",
+    "license": "AGPL-3",
+    "category": "",
+    "version": "14.0.1.0.0",
+    "depends": ["sms"],
+    "data": [
+        "data/ir_cron_data.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
