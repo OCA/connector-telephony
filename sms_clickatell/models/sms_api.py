703cb31b11b1c9b6a52a80a82ac27201005bf25e
RPSJR <raimundops.jr@gmail.com>
2021-07-18 02:30:32 -0300

    [pre-commit] pylint

--- sms_clickatell/models/sms_api.py
+++ sms_clickatell/models/sms_api.py
@@ -25,36 +25,12 @@ class SmsApi(models.AbstractModel):
         }
 
         if local_endpoint == "/iap/message_send":
-            """Send a single message to several numbers
-
-            :param numbers: list of E164 formatted phone numbers
-            :param message: content to send
-
-            :raises ? TDE FIXME
-            """
             messages = [
                 {"channel": "sms", "to": number, "content": params["message"]}
                 for number in params["numbers"]
             ]
 
         elif local_endpoint == "/iap/sms/1/send":
-            """Send SMS using IAP in batch mode
-
-            :param messages: list of SMS to send, structured as dict [{
-                'res_id':  integer: ID of sms.sms,
-                'number':  string: E164 formatted phone number,
-                'content': string: content to send
-            }]
-
-            :return: return of /iap/sms/1/send controller which is a list of dict [{
-                'res_id': integer: ID of sms.sms,
-                'state':  string: 'insufficient_credit' or
-                        'wrong_number_format' or 'success',
-                'credit': integer: number of credits spent to send this SMS,
-            }]
-
-            :raises: normally none
-            """
             messages = [
                 {
                     "channel": "sms",
@@ -66,6 +42,8 @@ class SmsApi(models.AbstractModel):
 
         values = json.dumps({"messages": messages})
         request = requests.post(
-            "https://platform.clickatell.com/v1/message", data=values, headers=headers,
+            "https://platform.clickatell.com/v1/message",
+            data=values,
+            headers=headers,
         )
         return request.json()
