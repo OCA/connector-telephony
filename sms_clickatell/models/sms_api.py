f155eff92d38216e5b5e756784a090fbc7ee1083
RPSJR <raimundops.jr@gmail.com>
2021-07-18 01:02:45 -0300

    [COR] E164 without + requiriment

--- sms_clickatell/models/sms_api.py
+++ sms_clickatell/models/sms_api.py
@@ -1,15 +1,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import json
+import logging
 
 import requests
 
 from odoo import api, models
 
+_logger = logging.getLogger(__name__)
+
 
 class Clickatell(object):
     # _name = "clickatell.sdk"
-    # _description = "Clickatell SDK to send SMS"
+    """Clickatell SDK to send SMS"""
 
     def __init__(self, key):
         self.headers = {
@@ -37,13 +40,24 @@ class SmsApi(models.AbstractModel):
 
     @api.model
     def _send_sms(self, numbers, message):
-        account = self.env["iap.account"].get("nexmo.sms")
+        """Send a single message to several numbers
+
+        :param numbers: list of E164 without + formatted phone numbers
+        (what standard is it?)
+        :param message: content to send
+
+        :raises ? TDE FIXME
+        """
+        account = self.env["iap.account"].get("clickatell.sms")
+        _logger.warning("account %s", account)
         if not account:
             return super(SmsApi, self)._send_sms(numbers, message)
         sms = Clickatell(key=account.key)
-        self._send_sms_clickatell(
-            sms, {"channel": "sms", "to": numbers, "content": message},
-        )
+        numbers = [number[1:] for number in numbers]
+        for number in numbers:
+            self._send_sms_clickatell(
+                sms, {"channel": "sms", "to": number, "content": message},
+            )
 
     @api.model
     def _send_sms_batch(self, messages):
@@ -52,13 +66,12 @@ class SmsApi(models.AbstractModel):
             return super(SmsApi, self)._send_sms_batch(messages)
 
         sms = Clickatell(key=account.key)
-
         for record in messages:
             result = self._send_sms_clickatell(
                 sms,
                 {
                     "channel": "sms",
-                    "to": record["number"],
+                    "to": record["number"][1:],
                     "content": record["content"],
                 },
             )
