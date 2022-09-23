3545b9acdd638546ff64d279679781bb2a5410d2
tsuarez <tsuarez@coninpe.es>
2022-09-23 09:51:08 +0200

    [IMP] Run pre-commit and reduce complexity on controller.py authenticate function

--- voz_telecom_connector/models/voztelecom_config.py
+++ voz_telecom_connector/models/voztelecom_config.py
@@ -1,50 +1,82 @@
-# -*- coding: utf:8 -*-
 # © 2022 CONINPE Consultores Informaticos: Telmo Suarez Venero <tsuarez@zertek.es>
 # License AGPL-3.0 or later (http://gnu.org/license/agpl.html).
 import requests
-from odoo import fields, models, exceptions, api
+
+from odoo import api, exceptions, fields, models
 
 
 class VozTelecomConfig(models.Model):
-	_name = "voztelecom.config"
-	_description = "VozTelecom API configuration model"
+    _name = "voztelecom.config"
+    _description = "VozTelecom API configuration model"
 
-	active_configuration = fields.Boolean(string="Activado")
-	name = fields.Char(string="Nombre")
-	api_token = fields.Char(string="VozTelecom API token")
-	callback_url = fields.Char(string="Dominio de respuesta para la API")
-	state = fields.Selection([
-		('done', 'Activo'),
-		('blocked', 'Desactivado')], string='Kanban State',
-		copy=False, default='blocked', required=True)
+    active_configuration = fields.Boolean(string="Activado")
+    name = fields.Char(string="Nombre")
+    api_token = fields.Char(string="VozTelecom API token")
+    callback_url = fields.Char(string="Dominio de respuesta para la API")
+    state = fields.Selection(
+        [("done", "Activo"), ("blocked", "Desactivado")],
+        string="Kanban State",
+        copy=False,
+        default="blocked",
+        required=True,
+    )
 
-	def api_configuration(self):
-		internal_api_key = self.env['res.users.apikeys']._generate(None, 'voz_telecom api key')
-		res = requests.post("https://config.work.oigaa.com/vtcrm/api/configure",
-							'{"events_callback_url":"' + self.callback_url + '/vtcrm/callback/"'
-																			 ',''"events_callback_headers":{'
-																			 '"user":"' + self.env.user.login + '","password":"' + internal_api_key + '","db":"' + self._cr.dbname + '"}}',
-							headers={"content-type": "json", "Authorization": "VTCRM-Basic " + self.api_token})
-		if res.status_code == 200:
-			self.state = 'done'
-		else:
-			raise exceptions.UserError(res.text)
+    def api_configuration(self):
+        internal_api_key = self.env["res.users.apikeys"]._generate(
+            None, "voz_telecom api key"
+        )
+        res = requests.post(
+            "https://config.work.oigaa.com/vtcrm/api/configure",
+            '{"events_callback_url":"' + self.callback_url + '/vtcrm/callback/"'
+            ","
+            '"events_callback_headers":{'
+            '"user":"'
+            + self.env.user.login
+            + '","password":"'
+            + internal_api_key
+            + '","db":"'
+            + self._cr.dbname
+            + '"}}',
+            headers={
+                "content-type": "json",
+                "Authorization": "VTCRM-Basic " + self.api_token,
+            },
+        )
+        if res.status_code == 200:
+            self.state = "done"
+        else:
+            raise exceptions.UserError(res.text)
 
-	@api.onchange('api_token', 'callback_url')
-	def reset_state(self):
-		self.state = 'blocked'
+    @api.onchange("api_token", "callback_url")
+    def reset_state(self):
+        self.state = "blocked"
 
-	def api_trigger_call(self, extension, destination):
-		requests.post("https://config.work.oigaa.com/vtcrm/api/call",
-					  '{"extension": "' + extension + '","destination": "' + destination + '"}',
-					  headers={"content-type": "json", "Authorization": "VTCRM-Basic " + self.api_token})
+    def api_trigger_call(self, extension, destination):
+        requests.post(
+            "https://config.work.oigaa.com/vtcrm/api/call",
+            '{"extension": "' + extension + '","destination": "' + destination + '"}',
+            headers={
+                "content-type": "json",
+                "Authorization": "VTCRM-Basic " + self.api_token,
+            },
+        )
 
-	def api_drop_call(self, callId):
-		requests.post("https://config.work.oigaa.com/vtcrm/api/drop",
-					  '{"callId": "' + callId + '"}',
-					  headers={"content-type": "json", "Authorization": "VTCRM-Basic " + self.api_token})
+    def api_drop_call(self, callId):
+        requests.post(
+            "https://config.work.oigaa.com/vtcrm/api/drop",
+            '{"callId": "' + callId + '"}',
+            headers={
+                "content-type": "json",
+                "Authorization": "VTCRM-Basic " + self.api_token,
+            },
+        )
 
-	def api_redirect_call(self, callId, destination):
-		requests.post("https://config.work.oigaa.com/vtcrm/api/redirectIncoming",
-					  '{"callId": "' + callId + '","destination": "' + destination + '"}',
-					  headers={"content-type": "json", "Authorization": "VTCRM-Basic " + self.api_token})
+    def api_redirect_call(self, callId, destination):
+        requests.post(
+            "https://config.work.oigaa.com/vtcrm/api/redirectIncoming",
+            '{"callId": "' + callId + '","destination": "' + destination + '"}',
+            headers={
+                "content-type": "json",
+                "Authorization": "VTCRM-Basic " + self.api_token,
+            },
+        )
