2fb763eb4f9447c90dc9b87d59be529772f0c1ae
Urvisha-OSI <udesai@opensourceintegrators.com>
2022-10-28 17:46:38 +0530

    [FIX] fixed pre-commit faild issues

--- asterisk_click2dial/static/src/js/asterisk_click2dial.js
+++ asterisk_click2dial/static/src/js/asterisk_click2dial.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 /*  Copyright 2014-2021 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
     Copyright 2015-2021 Juris Malinens (port to v9)
     License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).   */
