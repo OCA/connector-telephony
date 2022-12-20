1165b46b6faee18927898c0ae04a5d9062744b6d
George Smirnov <sombunall@gmail.com>
2022-12-20 23:37:08 +0300

    [MIG] base_phone: Migration to 15.0

--- base_phone/models/phone_validation_mixin.py
+++ base_phone/models/phone_validation_mixin.py
@@ -5,9 +5,12 @@
 
 from odoo import models
 
+from odoo.addons.phone_validation.tools import phone_validation
+
 
 class PhoneValidationMixin(models.AbstractModel):
-    _inherit = "phone.validation.mixin"
+    _name = "phone.validation.mixin"
+    _description = "Phone Validation Mixin"
 
     def _phone_get_country(self):
         if "country_id" in self and self.country_id:
@@ -15,3 +18,15 @@ class PhoneValidationMixin(models.AbstractModel):
         if "partner_id" in self and self.partner_id and self.partner_id.country_id:
             return self.partner_id.country_id
         return self.env.company.country_id
+
+    def phone_format(self, number, country=None, company=None):
+        country = country or self._phone_get_country()
+        if not country:
+            return number
+        return phone_validation.phone_format(
+            number,
+            country.code if country else None,
+            country.phone_code if country else None,
+            force_format="INTERNATIONAL",
+            raise_exception=False,
+        )
