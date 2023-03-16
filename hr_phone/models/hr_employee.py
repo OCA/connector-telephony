ea130f544df6a9e2d4e94d2acedd36d033892cd7
Minh Chien <amemberofuit@gmail.com>
2023-03-16 15:22:44 +0700

    [16.0][MIG] hr_phone: Migration to 16.0

--- hr_phone/models/hr_employee.py
+++ hr_phone/models/hr_employee.py
@@ -4,22 +4,37 @@
 
 from odoo import api, models
 
+from odoo.addons.phone_validation.tools import phone_validation
+
 
 class HrEmployeePrivate(models.Model):
-    _name = "hr.employee"
-    _inherit = ["hr.employee", "phone.validation.mixin"]
+    _inherit = "hr.employee"
     _phone_name_sequence = 30
-    _phone_name_fields = ["mobile_phone"]
-    # work_phone is now a computed field that take the value address_id.phone
-    # Don't put emergency_phone in _phone_name_fields because it is not a phone
-    # number of the employee
+
+    def _phone_get_number_fields(self):
+        # work_phone is now a computed field that take the value address_id.phone
+        # Don't put emergency_phone in _phone_name_fields because it is not a phone
+        # number of the employee
+        return ["mobile_phone"]
 
     @api.onchange("mobile_phone")
     def mobile_phone_change(self):
         if self.mobile_phone:
-            self.mobile_phone = self.phone_format(self.mobile_phone)
+            self.mobile_phone = self._phone_format(self.mobile_phone)
 
     @api.onchange("emergency_phone")
     def emergency_phone_change(self):
         if self.emergency_phone:
-            self.emergency_phone = self.phone_format(self.emergency_phone)
+            self.emergency_phone = self._phone_format(self.emergency_phone)
+
+    def _phone_format(self, number, country=None):
+        country = country or self.country_id or self.env.company.country_id
+        if not country:
+            return number
+        return phone_validation.phone_format(
+            number,
+            country.code if country else None,
+            country.phone_code if country else None,
+            force_format="INTERNATIONAL",
+            raise_exception=False,
+        )
--- hr_phone/readme/CONTRIBUTORS.rst
+++ hr_phone/readme/CONTRIBUTORS.rst
@@ -1,1 +1,5 @@
 * Alexis de Lattre <alexis.delattre@akretion.com>
+* `Trobz <https://trobz.com>`_:
+
+    * Hung Huynh <hunghn@trobz.com>
+    * Nguyễn Minh Chiến <chien@trobz.com>
