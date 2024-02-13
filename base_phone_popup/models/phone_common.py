92e2d693faa2a48fe80f4f7c789ad626e250d475
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2024-02-13 15:34:35 +0530

    [MIG] base_phone_popup

--- base_phone_popup/models/phone_common.py
+++ base_phone_popup/models/phone_common.py
@@ -6,7 +6,7 @@ import logging
 
 from odoo import _, api, models
 
-from odoo.addons.web.controllers.main import clean_action
+from odoo.addons.web.controllers.utils import clean_action
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +25,6 @@ class PhoneCommon(models.AbstractModel):
                 "res_model": record_res[0],
                 "view_mode": "form,tree",
                 "views": [[False, "form"]],
-                # 'target': 'new',
                 "res_id": record_res[1],
             }
         else:
@@ -35,7 +34,6 @@ class PhoneCommon(models.AbstractModel):
                 "res_model": "number.not.found",
                 "view_mode": "form",
                 "views": [[False, "form"]],
-                # 'target': 'new',
                 "context": {"default_calling_number": number},
             }
         return action
@@ -46,25 +44,24 @@ class PhoneCommon(models.AbstractModel):
         res = self.get_record_from_phone_number(number)
         users = self.env["res.users"].search([("login", "in", login_list)])
         logger.info(
-            "Notify incoming call from number %s to user IDs %s" % (number, users.ids)
+            f"Notify incoming call from number {number} to user IDs {users.ids}"
         )
         action = self._prepare_incall_pop_action(res, number)
-        action = clean_action(action)
+        action = clean_action(action, self.env)
         if action:
             for user in users:
-                channel = "notify_info_%s" % user.id
+                channel_partner = self.env.user.partner_id
                 bus_message = {
+                    "type": "success",
                     "message": _("Here is my message"),
                     "title": _("Incoming call"),
                     "action": action,
-                    # 'sticky': True,
                     "action_link_name": "action_link_name",
                 }
-
-                self.sudo().env["bus.bus"].sendone(channel, bus_message)
-                logger.debug(
-                    "This action has been sent to user ID %d: %s" % (user.id, action)
+                self.sudo().env["bus.bus"]._sendone(
+                    channel_partner, "web.notify", [bus_message]
                 )
+                logger.debug(f"This action has been sent to user ID {user.id} {action}")
         if res:
             callerid = res[2]
         else:
