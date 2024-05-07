67c4d9e4e8472443f04303df3cb866e04a71e1e4
Luis Rodriguez <luis.rodriguez@dixmit.com>
2024-05-07 11:18:24 +0200

    [MIG] asterisk_click2dial: Migration to 17.0

--- asterisk_click2dial/__manifest__.py
+++ asterisk_click2dial/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Asterisk connector",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Phone",
     "license": "AGPL-3",
     "summary": "Asterisk-Odoo connector",
