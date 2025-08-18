1869a814ca6055e21eca5e3f32c18da0cd212407
CarlosRoca13 <carlos.roca@tecnativa.com>
2025-08-18 12:54:33 +0200

    [IMP] voip_oca: Hide systray button when no provider defined on user

--- voip_oca/static/tests/components/ActivityTab.esm.test.js
+++ voip_oca/static/tests/components/ActivityTab.esm.test.js
@@ -6,6 +6,8 @@ import {click, start, startServer} from "@mail/../tests/mail_test_helpers";
 import {expect, test} from "@odoo/hoot";
 import {animationFrame} from "@odoo/hoot-dom";
 import {defineVoipModels} from "../voip_test_helpers.esm";
+import {patchWithCleanup} from "@web/../tests/web_test_helpers";
+import {session} from "@web/session";
 
 // As we use mail as a dependancy, we need to declare models.
 defineVoipModels();
@@ -29,6 +31,10 @@ test("Check Activity Tab", async () => {
         main_partner: "Test Partner",
         activity_category: "phonecall",
         activity_type_id: activityTypeId.id,
+    });
+    patchWithCleanup(session, {
+        ...session,
+        voip: {pbx_id: 1},
     });
     await start();
     expect(".o_menu_systray .o_nav_entry[title='Softphone']").toHaveCount(1);
